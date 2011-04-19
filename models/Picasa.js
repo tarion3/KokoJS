8e901b11c6493254878f5988b84ec355138941b8
Travis Ard <travis.ard@gmail.com>
2011-04-19 02:11:42 +0000

    Changed references from JSON to JSONP

--- models/Picasa.js
+++ models/Picasa.js
@@ -3,7 +3,7 @@ koko.defineModel('Picasa', function() {
     // return top 10 most recent photos
     this.getRecentPhotos = function(eventData, callback, context) {
         var username = eventData.username;
-        koko.loadJSON('https://picasaweb.google.com/data/feed/api/user/' + username + '?kind=photo&max-results=10&alt=json&callback=', callback, context, true);
+        koko.loadJSONP('https://picasaweb.google.com/data/feed/api/user/' + username + '?kind=photo&max-results=10&alt=json&callback=', callback, context, true);
     };
 
 });
