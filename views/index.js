125887aad52da09def70ca90ab30bd937fe9ae89
Travis Ard <travis.ard@gmail.com>
2018-10-10 16:31:58 -0500

    Update index.js
    Removed google maps view code

--- views/index.js
+++ views/index.js
@@ -1,17 +1,3 @@
-var googleMaps = koko.View('GoogleMaps', function() {
-            
-    koko.require('./adapters/GoogleMaps.js');
-
-    this.getMap = function(address) {
-        this.dispatchEvent('Adapter:GoogleMaps:getMap', {
-            data: { 'address': address, 'destCanvas': document.getElementById('map') },
-            callback: function(map) {},
-            onerror: function(error) { $('#map').attr('innerHTML', error); }
-        });
-    };
-
-});
-
 var twitter = koko.View('Twitter', function() {
             
     koko.require('./adapters/Twitter.js');
@@ -38,7 +24,6 @@ koko.require('https://code.jquery.com/jquery-1.12.4.min.js', {
     callback: function() {
 
         $(document).ready(function() {
-            $('#mapForm').submit(function() { googleMaps.getMap($('#mapname').attr('value')); return false; });
             $('#pubTweets').click(function() { twitter.getPublicTweets(); });
             $('#tweetForm').submit(function() { twitter.getTweetsByName($('#tweetUsername').attr('value')); return false; });
         });
